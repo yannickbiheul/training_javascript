3af16b14a63479f519213fe136bcb773c0da037b
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-13 09:52:12 +0200

    Ajout des spans

--- js/main.js
+++ js/main.js
@@ -3,10 +3,16 @@
 /* Déclaration des variables */
 let burgerButton = document.querySelector('.burgerButton');
 let burgerMenu = document.querySelector('.burgerMenu');
+let span1 = document.querySelector('.span1');
+let span2 = document.querySelector('.span2');
+let span3 = document.querySelector('.span3');
 
-/* Dévlaration de la fonction qui switch l'ouverture du menu */
+/* Déclaration de la fonction qui switch l'ouverture du menu */
 function menuOpen() {
     burgerMenu.classList.toggle('burgerMenuOpen');
+    span1.classList.toggle('span1Open');
+    span2.classList.toggle('span2Open');
+    span3.classList.toggle('span3Open');
 }
 
 /* Ajout d'un écouteur d'événement sur le bouton */

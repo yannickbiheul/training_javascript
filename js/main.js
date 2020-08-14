12fcda959bdb10d8c3fcdce9718c55fe1de07754
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-14 12:24:35 +0200

    Implémentation

--- js/main.js
+++ js/main.js
@@ -18,4 +18,32 @@ function menuOpen() {
 /* Ajout d'un écouteur d'événement sur le bouton */
 burgerButton.addEventListener('click', menuOpen);
 
+/* Valider un rendez-vous */
+
+// Exemple 1 : pour une durée de RDV de 15 minutes
+
+// Début du RDV : 15h40
+let debutRdvHeures = 15;
+let debutRdvMinutes = 40;
+
+// Fin de journée : 16h30
+let finJourneeHeures = 16;
+let finJourneeMinutes = 30;
+
+// Durée du rendez-vous : 15 minutes
+let duree = 15;
+
+// Étape 1 : Calcul de la fin du RDV
+let finRdvHeures = debutRdvHeures;
+let finRdvMinutes = debutRdvMinutes + duree;
+
+// Étape 2 : Est-ce que la fin du RDV est avant la fin de la journée ?
+if (finRdvHeures < finJourneeHeures) {
+    console.log("RDV OK")
+} else {
+    console.log("RDV impossible");
+}
+
+// Étape 3 : Affichage de l'heure de fin du RDV
+console.log("Le RDV se terminera à " + finRdvHeures + "h" + finRdvMinutes);
 

f8517359e75abfdb75cf4dd3f0e08f223b741c27
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-14 14:06:09 +0200

    Itération 1

--- js/main.js
+++ js/main.js
@@ -30,15 +30,24 @@ let debutRdvMinutes = 40;
 let finJourneeHeures = 16;
 let finJourneeMinutes = 30;
 
-// Durée du rendez-vous : 15 minutes
-let duree = 15;
+// Durée du rendez-vous 
+let duree = 55;
 
 // Étape 1 : Calcul de la fin du RDV
 let finRdvHeures = debutRdvHeures;
 let finRdvMinutes = debutRdvMinutes + duree;
 
+if (finRdvMinutes >= 60) {
+    finRdvHeures += 1;
+    finRdvMinutes -= 60;
+}
+
+if (finRdvMinutes < 10) {
+    finRdvMinutes = "0" + finRdvMinutes;
+}
+
 // Étape 2 : Est-ce que la fin du RDV est avant la fin de la journée ?
-if (finRdvHeures < finJourneeHeures) {
+if (finRdvHeures <= finJourneeHeures || finRdvMinutes < finJourneeMinutes) {
     console.log("RDV OK")
 } else {
     console.log("RDV impossible");

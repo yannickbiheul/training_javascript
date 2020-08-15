42bb88f6e64823a901aae73afbba61348e3ed758
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-15 08:46:00 +0200

    Essai avec changement de jour

--- js/main2.js
+++ js/main2.js
@@ -8,34 +8,57 @@ let finJourneeMinutes = 30;
 console.log("La journée se termine à " + finJourneeHeures + "h" + finJourneeMinutes);
 
 // Début du RDV : 15h40
+let rdv = "";
 let debutRdvHeures = 15;
 let debutRdvMinutes = 40;
 console.log("Le RDV débute à " + debutRdvHeures + "h" + debutRdvMinutes);
 
 // Durée du rendez-vous 
-let duree = 25;
+let duree = 1360;
 console.log("Le RDV dure " + duree + " minutes.");
 
 // Étape 1 : Calcul de la fin du RDV
 finRdvHeures = debutRdvHeures;
 finRdvMinutes = debutRdvMinutes + duree;
-if (finRdvMinutes >= 60) {
+finRdvJour = "";
+while (finRdvMinutes >= 60) {
     finRdvHeures += 1;
     finRdvMinutes -=60;
+}
+while (finRdvHeures >= 24) {
+    finRdvJour += 1;
+    finRdvHeures -= 24;
 }
 console.log("Heure de fin de RDV : " + finRdvHeures);
 console.log("Minutes de fin de RDV : " + finRdvMinutes);
 
 // Étape 3 : Est-ce que la fin du RDV est avant la fin de la journée ?
-if (finRdvHeures < finJourneeHeures || finRdvHeures == finJourneeHeures && finRdvMinutes < finJourneeMinutes) {
+if (finRdvHeures < finJourneeHeures || finRdvHeures == finJourneeHeures && finRdvMinutes <= finJourneeMinutes) {
     console.log("RDV OK");
+    rdv = true;
 } else {
     console.log("RDV impossible");
+    rdv = false;
 }
 
 // Étape 4 : Affichage de l'heure de fin du RDV
-if (finRdvMinutes < 10) {
-    console.log("Le RDV se terminera à " + finRdvHeures + "h0" + finRdvMinutes);
+if (rdv) {
+    if (finRdvMinutes < 10) {
+        console.log("Le RDV se terminera à " + finRdvHeures + "h0" + finRdvMinutes);
+    } else {
+        console.log("Le RDV se terminera à " + finRdvHeures + "h" + finRdvMinutes);
+    }
+} else if (finRdvJour) {
+    if (finRdvMinutes < 10) {
+        console.log("Le RDV se terminerait dans " + finRdvJour + " jours, à " + finRdvHeures + "h0" + finRdvMinutes);
+    } else {
+        console.log("Le RDV se terminerait dans " + finRdvJour + " jours, à " + finRdvHeures + "h" + finRdvMinutes);
+    }
 } else {
-    console.log("Le RDV se terminera à " + finRdvHeures + "h" + finRdvMinutes);
+    if (finRdvMinutes < 10) {
+        console.log("Le RDV se terminerait à " + finRdvHeures + "h0" + finRdvMinutes);
+    } else {
+        console.log("Le RDV se terminerait à " + finRdvHeures + "h" + finRdvMinutes);
+    }
 }
+

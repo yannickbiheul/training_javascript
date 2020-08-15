9f5447deec34b1038af16eb6da7dd86417a18ab4
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-15 08:29:14 +0200

    Nouvelle version

--- js/main2.js
+++ js/main2.js
@@ -2,55 +2,40 @@
 
 // Exemple 1 : pour une durée de RDV de 15 minutes
 
-// Début du RDV : 15h40
-let debutRdvHeures = 15;
-let debutRdvMinutes = 40;
-console.log("Le RDV débute à " + debutRdvHeures + "h" + debutRdvMinutes);
-
 // Fin de journée : 16h30
 let finJourneeHeures = 16;
 let finJourneeMinutes = 30;
 console.log("La journée se termine à " + finJourneeHeures + "h" + finJourneeMinutes);
+
+// Début du RDV : 15h40
+let debutRdvHeures = 15;
+let debutRdvMinutes = 40;
+console.log("Le RDV débute à " + debutRdvHeures + "h" + debutRdvMinutes);
 
 // Durée du rendez-vous 
 let duree = 25;
 console.log("Le RDV dure " + duree + " minutes.");
 
 // Étape 1 : Calcul de la fin du RDV
-let finRdvHeures = debutRdvHeures;
-let finRdvMinutes = debutRdvMinutes + duree;
+finRdvHeures = debutRdvHeures;
+finRdvMinutes = debutRdvMinutes + duree;
 if (finRdvMinutes >= 60) {
     finRdvHeures += 1;
-    finRdvMinutes -= 60;
-    if (finRdvMinutes < 10) {
-        finRdvMinutes = "0" + finRdvMinutes;
-    }
+    finRdvMinutes -=60;
 }
-console.log("Le RDV se termine à " + finRdvHeures + "h" + finRdvMinutes);
+console.log("Heure de fin de RDV : " + finRdvHeures);
+console.log("Minutes de fin de RDV : " + finRdvMinutes);
 
 // Étape 3 : Est-ce que la fin du RDV est avant la fin de la journée ?
-
-if (finRdvHeures <= finJourneeHeures) {
-    console.log("L'heure de fin du rendez-vous est inférieure ou égale à l'heure de fin de journée.");
-}
-else {
-    console.log("L'heure de fin du rendez-vous est supérieure à l'heure de fin de journée");
-}
-
-if (finRdvMinutes > finJourneeMinutes && finRdvHeures >= finJourneeHeures) {
-    console.log("L'heure est bonne mais les minutes sont supérieures");
+if (finRdvHeures < finJourneeHeures || finRdvHeures == finJourneeHeures && finRdvMinutes < finJourneeMinutes) {
+    console.log("RDV OK");
+} else {
     console.log("RDV impossible");
-} else if (finRdvMinutes > finJourneeMinutes && finRdvHeures < finJourneeMinutes) {
-    console.log("Les minutes sont supérieures, mais l'heure est bonne.");
-    console.log("RDV OK");
-} else if (finRdvMinutes < finJourneeMinutes && finRdvHeures >= finJourneeHeures) {
-    console.log("Les minutes sont inférieures mais l'heure est bonne.");
-    console.log("RDV OK");
-} else if (finRdvMinutes == finJourneeMinutes && finRdvHeures >= finJourneeHeures) {
-    console.log("Les minutes sont égales et l'heure est bonne.");
-    console.log("RDV OK");
 }
-
 
 // Étape 4 : Affichage de l'heure de fin du RDV
-// console.log("Le RDV se terminera à " + finRdvHeures + "h" + finRdvMinutes);
+if (finRdvMinutes < 10) {
+    console.log("Le RDV se terminera à " + finRdvHeures + "h0" + finRdvMinutes);
+} else {
+    console.log("Le RDV se terminera à " + finRdvHeures + "h" + finRdvMinutes);
+}

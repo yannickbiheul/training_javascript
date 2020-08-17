e718d603a6d4be14508e97cd61dda240f3fa6031
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-17 17:33:03 +0200

    Boucle do while

--- js/boucles.js
+++ js/boucles.js
@@ -1,7 +1,17 @@
+// BOUCLE WHILE
 let compteur = 0;
 while (compteur < 3) {
     console.log("Compteur : " + compteur);
     console.log("Instruction de la boucle while");
     compteur++;
 }
-console.log("Fin de la boucle");
+console.log("Fin de la boucle while");
+
+// BOUCLE DO WHILE
+let compteur2 = 0;
+do {
+    console.log("Compteur : " + compteur2);
+    console.log("Instruction de la boucle do while");
+    compteur2++;
+} while (compteur2 < 3);
+console.log("Fin de la boucle do while");

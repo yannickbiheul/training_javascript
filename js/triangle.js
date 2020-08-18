670c4dd1389f3777dc7248860a201b9c90299629
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-18 12:57:12 +0200

    Ajout de commentaires

--- js/triangle.js
+++ js/triangle.js
@@ -2,19 +2,32 @@
 
 // Fonction principale
 function triangle(largeurTriangle) {
+    // Ajouter une ligne vide
     let ligne = "";
+    // Augmenter le nombre d'étoiles
+    // Créer une boucle qui déterminera le nombre de lignes
     for (let longueurLigne = 1; longueurLigne <= largeurTriangle; longueurLigne += 1) {
+        // Créer une boucle qui déterminera le nombre d'étoiles
         for (let position = 0; position < longueurLigne; position += 1) {
+            // Ajouter une étoile dans la ligne
             ligne += "*";
         }
+        // Afficher les étoiles ajoutées dans la console
         console.log(ligne);
+        // Effacer la ligne
         ligne = "";
     }
+    // Diminuer le nombre d'étoiles
+    // Créer une boucle qui déterminera le nombre de lignes
     for (let longueurLigne = largeurTriangle - 1; longueurLigne > 0; longueurLigne -= 1) {
+        // Créer une boucle qui déterminera le nombre d'étoiles
         for (let position = 0; position < longueurLigne; position += 1) {
+            // Ajouter une étoile dans la ligne
             ligne += "*";
         }
+        // Afficher les étoiles ajoutées dans la console
         console.log(ligne);
+        // Effacer la ligne
         ligne = "";
     }
 }

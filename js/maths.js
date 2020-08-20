38d5474e6d73ce76a9a9c33878674ad849484788
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-20 11:23:25 +0200

    Ajout des fichiers html et js

---
+++ js/maths.js
@@ -0,0 +1,46 @@
+// Calculer et afficher la moyenne de nombres saisis au clavier.
+
+// Variables
+let enterNumber = document.querySelector('.enterNumber');
+let boutonOk = document.querySelector('.ok');
+let boutonStop = document.querySelector('.stop');
+let boutonReset = document.querySelector('.reset');
+let result = document.querySelector('.result');
+let result2 = document.querySelector('.result2');
+let number;
+let somme = 0;
+let compteur = 0;
+
+// Fonctions
+function ajouterNombre() {
+    result2.innerHTML = "";
+    number = parseInt(enterNumber.value);
+    result.innerHTML += number + " ";
+    somme += number;
+    // console.log(number + ", la somme est de : " + somme);
+    enterNumber.value = "";
+    compteur++;
+}
+
+function calculMoyenne() {
+    let moyenne = somme / compteur;
+    if (!moyenne) {
+        result2.innerHTML = "Vous n'auriez pas oublié quelque chose ?";
+    } else {
+        result2.innerHTML = "La moyenne est de : " + moyenne;
+    } 
+}
+
+function reset() {
+    number = 0;
+    somme = 0;
+    compteur = 0;
+    moyenne = 0;
+    result.innerHTML = "";
+    result2.innerHTML = "";
+}
+
+// Écouteurs d'événements
+boutonOk.addEventListener('click', ajouterNombre);
+boutonStop.addEventListener('click', calculMoyenne);
+boutonReset.addEventListener('click', reset);

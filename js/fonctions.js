e633c172225fdfca26ce75e6e378a2bb508ac031
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-21 11:11:46 +0200

    Ajout de fonctions.js

---
+++ js/fonctions.js
@@ -0,0 +1,20 @@
+
+// let ligne = "";
+
+// function afficherPlusieursEtoiles(nombre) {
+//     for (let position = 0; position <= nombre; position = position + 1) {
+//         ligne = ligne + "*";
+//     }
+//     console.log(ligne);
+// }
+
+// function afficherTriangleEtoile(longueurMax) {
+//     for (let position = 0; position < longueurMax; position = position + 1) {
+//         afficherPlusieursEtoiles(position);
+//     }
+//     for (let position = longueurMax - 2; position > 0; position = position - 2) {
+//         afficherPlusieursEtoiles(position);
+//     }
+// }
+
+// afficherTriangleEtoile(7);

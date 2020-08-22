cfc476177962a8bb5ac1ebf64ff0159da6f4a153
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-22 17:41:08 +0200

    Affichage d'étoiles en ordre croissant

--- js/fonctions.js
+++ js/fonctions.js
@@ -1,3 +1,23 @@
-// Afficher un triangle sur la console
+// Affichage un triangle sur la console
 
+/* Affichage d'une étoile */
+// function afficherEtoile() {
+//     console.log("*");
+// }
+// afficherEtoile();
 
+/* Affichage de plusieurs étoiles */
+// function afficherEtoiles(nombre) {
+//     let ligne = "";
+//     for (let i = 0; i < nombre; i++) {
+//         ligne += "*";
+//     }
+//     console.log(ligne);
+// }
+// afficherEtoiles(10);
+
+/* Affichage de plusieurs étoiles en ordre croissant */
+function croissant(nombre) {
+    let ligne = "";
+    
+}

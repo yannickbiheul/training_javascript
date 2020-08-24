6a46758af292d677360cc77e464c2d918813b4c1
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-24 16:01:32 +0200

    Triangle 1er essai

--- js/fonctions.js
+++ js/fonctions.js
@@ -6,34 +6,18 @@
 // }
 // afficherEtoile();
 
-/* Affichage de plusieurs étoiles */
-// function afficherEtoiles(nombre) {
-//     let ligne = "";
-//     for (let i = 0; i < nombre; i++) {
-//         ligne += "*";
-//     }
-//     console.log(ligne);
-// }
-// afficherEtoiles(10);
-
 /* Affichage de plusieurs étoiles en ordre croissant */
-function croissant(nombre) {
-    let etoiles = "";
-    for (let ligne = 0; ligne < nombre; ligne++) {
-        etoiles += "*";
-        console.log(etoiles);
+
+function triangle(largeurTriangle) {
+    let ligne = "";
+    for (let positionLigne = 0; positionLigne < largeurTriangle; positionLigne++) {
+        for (let position = 0; position <= positionLigne; position++) {
+            ligne += "*";
+            console.log(ligne);
+        }
+        ligne = "";
     }
 }
-
-croissant(5);
+triangle(5);
 
 /* Affichage de plusieurs étoiles en ordre décroissant */
-// function decroissant(nombre) {
-//     let etoiles = "";
-//     for (let ligne = nombre; ligne >= 0; ligne--) {
-//         etoiles += "*";
-//         console.log(etoiles);
-//     }
-// }
-
-// decroissant(5);

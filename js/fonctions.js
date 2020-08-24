90bf03f897102ac04ea08a53af74d1d6c9cc84b1
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-24 16:10:52 +0200

    Fonction triangleCroissant réussie

--- js/fonctions.js
+++ js/fonctions.js
@@ -8,16 +8,16 @@
 
 /* Affichage de plusieurs étoiles en ordre croissant */
 
-function triangle(largeurTriangle) {
+function triangleCroissant(largeurTriangle) {
     let ligne = "";
     for (let positionLigne = 0; positionLigne < largeurTriangle; positionLigne++) {
         for (let position = 0; position <= positionLigne; position++) {
             ligne += "*";
-            console.log(ligne);
         }
+        console.log(ligne);
         ligne = "";
     }
 }
-triangle(5);
+triangleCroissant(10);
 
 /* Affichage de plusieurs étoiles en ordre décroissant */

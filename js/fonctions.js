167c652d7b2859a0005a1004eb07819089eaa449
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-25 15:54:10 +0200

    Reconstruction fonction triangleDecroissant

--- js/fonctions.js
+++ js/fonctions.js
@@ -10,10 +10,20 @@ function triangleCroissant(largeur) {
         ligne = "";
     }
 }
-
+triangleCroissant(5);
 
 /* Affichage de plusieurs étoiles en ordre décroissant */
-
+function triangleDecroissant(largeur) {
+    let ligne = "";
+    for (let positionLigne = largeur - 1; positionLigne > 0; positionLigne--) {
+        for (let position = 0; position < positionLigne; position++) {
+            ligne+= "*";
+        }
+        console.log(ligne);
+        ligne = "";
+    }
+}
+triangleDecroissant(5);
 
 
 /* Affichage des 2 triangles à la suite */

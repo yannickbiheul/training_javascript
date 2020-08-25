c2f3411e20baaabd7b781e71570a70f64404443a
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-25 15:41:52 +0200

    Reconstruction fonction triangleCroissant

--- js/fonctions.js
+++ js/fonctions.js
@@ -1,11 +1,10 @@
 
 /* Affichage de plusieurs étoiles en ordre croissant */
-
-function triangleCroissant(largeurTriangle) {
+function triangleCroissant(largeur) {
     let ligne = "";
-    for (let positionLigne = 0; positionLigne < largeurTriangle; positionLigne++) {
+    for (let positionLigne = 0; positionLigne < largeur; positionLigne++) {
         for (let position = 0; position <= positionLigne; position++) {
-            ligne += "*";
+            ligne+= "*";
         }
         console.log(ligne);
         ligne = "";
@@ -15,23 +14,7 @@ function triangleCroissant(largeurTriangle) {
 
 /* Affichage de plusieurs étoiles en ordre décroissant */
 
-function triangleDecroissant(largeurTriangle) {
-    let ligne = "";
-    for (let positionLigne = largeurTriangle - 1; positionLigne > 0; positionLigne--) {
-        for (let position = 0; position < positionLigne; position++) {
-            ligne += "*";
-        }
-        console.log(ligne);
-        ligne = "";
-    }
-}
 
 
 /* Affichage des 2 triangles à la suite */
 
-function triangle(largeur) {
-    triangleCroissant(largeur);
-    triangleDecroissant(largeur);
-}
-
-triangle(10);

19ddedc5746694b56dca496991eac3d77026354e
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-27 16:57:02 +0200

    Ajout de commentaires

--- js/toDoList.js
+++ js/toDoList.js
@@ -1,6 +1,8 @@
+/* Stockage des éléments dans des variables */
 let div = document.querySelectorAll('.list');
 let suppr = document.querySelectorAll('.suppr');
 
+/* Création d'une boucle pour parcourir les éléments de la NodeList */
 for (let i = 0; i < suppr.length; i++) {
     suppr[i].addEventListener('click', function() {
         div[i].style.opacity = "0";

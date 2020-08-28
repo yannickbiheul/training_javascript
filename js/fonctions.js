a70fdbc64eb3435d735b3270356bfc0b104ae987
Yannick Biheul <yannickbiheul@outlook.fr>
2020-08-28 16:41:32 +0200

    Triangle croissant

--- js/fonctions.js
+++ js/fonctions.js
@@ -1,5 +1,16 @@
 /* Affichage de plusieurs étoiles en ordre croissant */
+function triangle(largeur) {
+    let ligne = "";
+    for (let positionLigne = 0; positionLigne < largeur; positionLigne++) {
+        for (let position = 0; position <= positionLigne; position++) {
+            ligne+= "* " + (position + 1) + " ";
+        }
+        console.log(ligne);
+        ligne = "";
+    }
+}
 
+triangle(10);
 
 
 /* Affichage de plusieurs étoiles en ordre croissant et pairs */

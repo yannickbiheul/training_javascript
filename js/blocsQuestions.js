bf0ae1d7d6522cd15eecc09b967bc80fb0d748c0
Yannick Biheul <yannickbiheul@outlook.fr>
2020-10-31 11:30:06 +0100

    Création du premier objet

--- js/blocsQuestions.js
+++ js/blocsQuestions.js
@@ -1,2 +1,27 @@
-let elts = document.querySelectorAll(".elt");
-console.log(elts);
+// BLOCS QUESTIONS
+
+let blocQuestion1 = document.querySelector(".blocQuestion1");
+let blocQuestion2 = document.querySelector(".blocQuestion2");
+let blocQuestion3 = document.querySelector(".blocQuestion3");
+let blocQuestion4 = document.querySelector(".blocQuestion4");
+let blocQuestion5 = document.querySelector(".blocQuestion5");
+let blocQuestion6 = document.querySelector(".blocQuestion6");
+let blocQuestion7 = document.querySelector(".blocQuestion7");
+let blocQuestion8 = document.querySelector(".blocQuestion8");
+let blocQuestion9 = document.querySelector(".blocQuestion9");
+
+let blocsQuestions = [blocQuestion1, blocQuestion2, blocQuestion3, blocQuestion4, blocQuestion5, blocQuestion6, blocQuestion7, blocQuestion8, blocQuestion9];
+console.log(blocsQuestions);
+
+
+
+// QUESTIONS
+
+let question1 = {
+    titre: "La question 1",
+    reponse1: "La réponse1",
+    reponse2: "La réponse 2",
+    reponse3: "La réponse 3",
+    reponse4: "La réponse 4",
+    bonneReponse: "La reponse 2"
+}
